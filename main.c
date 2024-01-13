a9dcc59ac434f87e843b602966e9727314bffc65
okezieuc <53785400+okezieuc@users.noreply.github.com>
2024-01-13 13:31:05 -0600

    write function to handle game end statuses

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 void name(int card_number);
 void value(int card_number);
+void end_status(int hand_value);
 
 int main()
 {
@@ -53,22 +54,56 @@ void name(int card_number)
     Parameters:
     - card_number: The number of the card whose value is to be printed
 */
-void value(int card_number ) {
-    if (card_number < 1 || card_number > 13) {
+void value(int card_number)
+{
+    if (card_number < 1 || card_number > 13)
+    {
         printf("BAD CARD");
         return;
     }
 
-    switch(card_number) {
-        case 1:
-            printf("Your hand value is 11");
-            return;
-        case 11:
-        case 12:
-        case 13:
-            printf("Your hand value is 10");
-            return;
-        default:
-            printf("Your hand value is %d\n", card_number);
+    switch (card_number)
+    {
+    case 1:
+        printf("Your hand value is 11");
+        return;
+    case 11:
+    case 12:
+    case 13:
+        printf("Your hand value is 10");
+        return;
+    default:
+        printf("Your hand value is %d\n", card_number);
+    }
+}
+
+/*
+    This function recevies the current hand value. If the hand value is not
+    possible, it prints "BAD HAND VALUE!" It prints "BLACKJACK!" if the hand
+    value is 21, "BUST!" if the hand value is greater than 21, and prints
+    nothing otherwise.
+
+    Parameters:
+    - hand_value: a player's hand value
+*/
+
+void end_status(int hand_value)
+{
+    if (hand_value < 4 || hand_value > 31)
+    {
+        printf("BAD HAND VALUE!");
+        return;
+    }
+
+    if (hand_value == 21)
+    {
+        printf("BLACJACK!");
+        return;
+    }
+
+    if (hand_value > 21)
+    {
+        printf("BUST");
+        return;
     }
 }
